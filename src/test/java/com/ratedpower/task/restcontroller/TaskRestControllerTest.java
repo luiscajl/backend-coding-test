28c5eb69b04b7cac6d3ef2868779647203f507cb
Luis Carlos <luisca_jl@hotmail.com>
2021-09-09 15:22:57 +0200

    Fixed test

--- src/test/java/com/ratedpower/task/restcontroller/TaskRestControllerTest.java
+++ src/test/java/com/ratedpower/task/restcontroller/TaskRestControllerTest.java
@@ -1,62 +1,27 @@
 package com.ratedpower.task.restcontroller;
 
-import com.ratedpower.task.TaskApplication;
-import com.ratedpower.task.service.TaskService;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
 
 @SpringBootTest
-@WebMvcTest(TaskRestController.class)
+@AutoConfigureMockMvc
 public class TaskRestControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    // @Test
-    // public void getAllEmployeesAPI() throws Exception {
-    //     mockMvc.perform(MockMvcRequestBuilders.get("tasks/", 1).accept(MediaType.APPLICATION_JSON)).andDo(print())
-    //             .andExpect(status().isOk()).andExpect(MockMvcResultMatchers.jsonPath("$.employees").exists())
-    //             .andExpect(MockMvcResultMatchers.jsonPath("$.employees[*].employeeId").isNotEmpty());
-    // }
-
     @Test
-    void testGetTask(String birthday, String dow) throws Exception {
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("tasks/", 1)
-                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-                .andReturn();
-
-        String resultDOW = result.getResponse().getContentAsString();
-        assertNotNull(resultDOW);
-        assertEquals(dow, resultDOW);
+    void testGetAllTasks() throws Exception {
+        this.mockMvc.perform(get("/tasks")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("[]")));
     }
 }
